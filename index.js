de1795f29fca552f8d91c978a3a4062a109e3959
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-06-24 17:25:48 -0500

    It works in replt

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+var bodyParser = require('body-parser');
 
 app.use(express.static('static'));
+app.use(express.json());
+app.use(bodyParser.text());
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   console.log("get /");
-  // res.sendFile(path.resolve('pages/index.html'));
+  res.sendFile(path.resolve('pages/index.html'));
+});
+
+app.post('/', (req, res) => {
+  console.log("post /", req.body);
+  res.send(req.body);
 });
 
 app.listen(port, () => {
