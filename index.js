a756e72fe7b3d54c856e1e3d3d8e67c1710ed2c6
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-06-28 12:06:46 -0500

    Blob response

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const net = require('net')
 const port = 80
 const server = net.createServer()
-let response_write = (data, dtype='hex') => {
-  console.log(data.toString(dtype))
-  return '01'
+let response_value = (data) => { 
+  response_any = default_response = '01'
+  console.log('<<--', response_any)
+  return default_response
+}
+let response_write = (data, dtype='hex', options={type: 'text/plain'}) => {
+  console.log(' \nREQ:', data.toString(dtype))
+  return new Blob([response_value(data)], options)
 }
 server.on('connection', (socket) => {
   socket.on('data', (data) => {
-    console.log('\nCliente: ' , `${socket.remoteAddress} : ${socket.remotePort} \n`)
+    console.log('\nCliente: ' , `${socket.remoteAddress} : ${socket.remotePort}`)
     socket.write(response_write(data))
-    console.log( "\n AT: ", new Date() )
+    console.log( "\nAT: ", new Date() , "\nRES: ", response_write(data))
   })
 
   socket.on('close', () => {
@@ -17,10 +22,10 @@ server.on('connection', (socket) => {
   })
 
   socket.on('error', (err) => {
-    console.log(err.message)
+    console.log('error en socket: ', err.message)
   })
 })
 
 server.listen(port, () => {
-  console.log('servidor esta escuchando en la puerta', server.address().port)
+  console.log('servidor esta escuchando en puerto', server.address().port)
 })
