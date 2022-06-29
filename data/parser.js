dbab57991c2b64158fa7f0302bf485df1b78623b
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-06-29 11:09:11 -0500

    dataparser: Exception & returning object[9]

--- data/parser.js
+++ data/parser.js
@@ -1,5 +1,13 @@
 let read_block = (bufferBlock) => {
-    console.log("MOD::read_block ", typeof bufferBlock)
-    return true
+    console.log("MOD::read_block? ", typeof bufferBlock)
+    block_success = true
+    try {
+        block_success = bufferBlock[9]
+        console.log("MOD::read_block-> ", block_success, typeof block_success)
+    }catch (e){
+        console.error("MOD::read_block[ERR] ", e)
+    }
+    console.log("MOD::read_block?? ", typeof block_success)
+    return block_success
 }
-module.exports.blockParser = read_block;
+module.exports.blockParser = read_block
