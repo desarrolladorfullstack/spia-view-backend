f0f0c4b2d2fe6d22e81f69b18ac707813fee5e70
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-06-29 10:56:05 -0500

    log dataparser (TEST 1)

--- data/parser.js
+++ data/parser.js
@@ -1,5 +1,5 @@
 let read_block = (bufferBlock) => {
-    console.log(typeof bufferBlock)
+    console.log("MOD::read_block ", typeof bufferBlock)
     return true
 }
 module.exports.blockParser = read_block;
