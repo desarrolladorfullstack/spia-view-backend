377b1bf42340e5f06367d05254dd9eda89235674
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-06-29 11:23:38 -0500

    analyse block (0.0.2)

--- data/parser.js
+++ data/parser.js
@@ -1,8 +1,17 @@
-let read_block = (bufferBlock) => {
+var IMEI_BLOCK_INDEX = '000f';
+const analyse_block = (bufferBlock) => {  
+    let hexBlock = bufferBlock.toString('hex')
+    console.log("MOD::analyse_block? ", typeof hexBlock)
+    if (hexBlock.indexOf(IMEI_BLOCK_INDEX) === 0){
+        return true
+    }
+    return bufferBlock[9]
+}
+const read_block = (bufferBlock) => {
     console.log("MOD::read_block? ", typeof bufferBlock)
     let block_success = true
     try {
-        block_success = bufferBlock[9]
+        block_success = analyse_block(bufferBlock.toString('hex'))
         console.log("MOD::read_block-> ", block_success, typeof block_success)
     }catch (e){
         console.error("MOD::read_block[ERR] ", e)
