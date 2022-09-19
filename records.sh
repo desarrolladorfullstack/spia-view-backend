2382be5069d08fc2f03a2431ff35fcf3b7b7d8cb
desarrolladorfullstack <desarrolladorweb@spia.co>
2022-09-19 11:19:30 -0500

    records.sh read lines by file (TRY)

--- records.sh
+++ records.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# :: Please run only with bash ..
 # TASKS:
 # 1. read files from media
 # 2. loop file contents
@@ -8,5 +9,16 @@ MEDIA_FOLDER="/home/node/media/"
 list_media_files=($(ls $MEDIA_FOLDER))
 for file in ${list_media_files[*]}
 do
-    echo $file
+    $input=$MEDIA_FOLDER$file
+    line_offset=0
+    echo "()=>$input reading ... \n"
+    { 
+        while IFS= read -r line
+        do
+            echo "$line"
+            line_offset=$((line_offset + 1))
+        done < "$input"
+    } || {
+        echo "error on $input:$line_offset\n"
+    }
 done
