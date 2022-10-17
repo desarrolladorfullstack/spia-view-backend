69ed0b45a3740a5dfaf2efddb38333db1023c5cd
desarrolladorfullstack <desarrolladorweb@spia.co>
2022-10-16 22:43:57 -0500

    rescue for loop $input file

--- records.sh
+++ records.sh
@@ -31,52 +31,57 @@ PGSQL_TABLE_PARENT_SEQUENCE="files_file_id_seq"
 list_media_files=($(ls $MEDIA_FOLDER))
 for file in ${list_media_files[*]}
 do
-    if [[ "$file" == "temp_file_raw"* ]]
-    then
-        continue
-    fi
-    input=$MEDIA_FOLDER$file
-    line_offset=0
-    record_offset=0
-    IFS='_' read -ra file_data_split <<< "$file"
-    device_id='00030efafb4bd16a7c000400'
-    timestamp=$(date '+%s')"000"
-    if [[ "$file" -ne "file_raw" ]]
-    then
-        echo "file_data_split: "${file_data_split[*]}
-        device_id=${file_data_split[-2]}
-        timestamp=${file_data_split[-3]}
-    fi
-    mime_type=$(file --mime-type $input)
-    IFS=': ' read -ra mime_type <<< $mime_type
-    mime_type=${mime_type[1]}
-    echo "()=>$input [$device_id, $timestamp] reading ... \n"
-    echo "INSERT INTO $PGSQL_TABLE_PARENT_NAME ($PGSQL_PARENT_COLUMN) VALUES ('$device_id', '$timestamp','$mime_type');" > $SQL_FOLDER"temp_insert.sql"
-    cat $SQL_FOLDER"temp_insert.sql"
-    psql -h $PGSQL_HOST -U $PGSQL_USER -d $PGSQL_DBNAME -p $PGSQL_PORT -f $SQL_FOLDER"temp_insert.sql"
-    { 
-        # while IFS= read -r line 
-        # 8a4a280168a4a2800a28a28016928a2800a28a2800a28a2800a28a4a0028 while join < 1024*2 chars
-        # do
-        lines_insert=($(xxd -p $input))
-        line_count=0
-        block=""
-        for line in ${lines_insert[*]}
-        do
-            if ((${#block} < ($BYTEBLOCK_LIMIT*2)))
-            then
-                block+=$line
-                continue
-            fi
-            echo "INSERT INTO $PGSQL_TABLE_NAME ($PGSQL_COLUMN) VALUES ('$block', $record_offset);" > $SQL_FOLDER"temp_insert.sql"
-            echo "INSERT INTO $PGSQL_TABLE_CROSS_NAME ($PGSQL_CROSS_COLUMN) SELECT sq1.last_value, sq2.last_value FROM $PGSQL_TABLE_PARENT_SEQUENCE sq1, $PGSQL_TABLE_SEQUENCE sq2;" >> $SQL_FOLDER"temp_insert.sql"
-            cat $SQL_FOLDER"temp_insert.sql"
-            psql -h $PGSQL_HOST -U $PGSQL_USER -d $PGSQL_DBNAME -p $PGSQL_PORT -f $SQL_FOLDER"temp_insert.sql"
-            line_offset=$((line_offset + 1))
+    {
+        if [[ "$file" == "temp_file_raw"* ]]
+        then
+            continue
+        fi
+        input=$MEDIA_FOLDER$file
+        line_offset=0
+        record_offset=0
+        IFS='_' read -ra file_data_split <<< "$file"
+        device_id='00030efafb4bd16a7c000400'
+        timestamp=$(date '+%s')"000"
+        if [[ "$file" -ne "file_raw" ]]
+        then
+            echo "file_data_split: "${file_data_split[*]}
+            device_id=${file_data_split[-2]}
+            timestamp=${file_data_split[-3]}
+        fi
+        mime_type=$(file --mime-type $input)
+        IFS=': ' read -ra mime_type <<< $mime_type
+        mime_type=${mime_type[1]}
+        echo "()=>$input [$device_id, $timestamp] reading ... \n"
+        echo "INSERT INTO $PGSQL_TABLE_PARENT_NAME ($PGSQL_PARENT_COLUMN) VALUES ('$device_id', '$timestamp','$mime_type');" > $SQL_FOLDER"temp_insert.sql"
+        cat $SQL_FOLDER"temp_insert.sql"
+        psql -h $PGSQL_HOST -U $PGSQL_USER -d $PGSQL_DBNAME -p $PGSQL_PORT -f $SQL_FOLDER"temp_insert.sql"
+        { 
+            # while IFS= read -r line 
+            # 8a4a280168a4a2800a28a28016928a2800a28a2800a28a2800a28a4a0028 while join < 1024*2 chars
+            # do
+            lines_insert=($(xxd -p $input))
+            line_count=0
             block=""
-        done
-        # done < "$input"
+            for line in ${lines_insert[*]}
+            do
+                if ((${#block} < ($BYTEBLOCK_LIMIT*2)))
+                then
+                    block+=$line
+                    continue
+                fi
+                echo "INSERT INTO $PGSQL_TABLE_NAME ($PGSQL_COLUMN) VALUES ('$block', $record_offset);" > $SQL_FOLDER"temp_insert.sql"
+                echo "INSERT INTO $PGSQL_TABLE_CROSS_NAME ($PGSQL_CROSS_COLUMN) SELECT sq1.last_value, sq2.last_value FROM $PGSQL_TABLE_PARENT_SEQUENCE sq1, $PGSQL_TABLE_SEQUENCE sq2;" >> $SQL_FOLDER"temp_insert.sql"
+                cat $SQL_FOLDER"temp_insert.sql"
+                psql -h $PGSQL_HOST -U $PGSQL_USER -d $PGSQL_DBNAME -p $PGSQL_PORT -f $SQL_FOLDER"temp_insert.sql"
+                line_offset=$((line_offset + 1))
+                block=""
+            done
+            # done < "$input"
+            rm $input
+        } || {
+            echo "error on Subprocess $input:$line_offset\n"
+        }
     } || {
-        echo "error on $input:$line_offset\n"
+            echo "error on Process $input\n"
     }
 done
