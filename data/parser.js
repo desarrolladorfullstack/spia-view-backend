11f0d536d4b100208f53b438f2468cf0e3153c7b
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-12-06 18:28:17 -0500

    settings TEST: parseInt raddix 16

--- data/parser.js
+++ data/parser.js
@@ -500,7 +500,7 @@ const analyse_block = (bufferBlock) => {
         try {
             let imei_byte_start = CAM_INIT_BYTE_LENGTH + IMEI_BYTE_LENGTH;
             let settings = bufferBlock.subarray(imei_byte_start, imei_byte_start + CAM_SETTINGS_BYTE_LENGTH)
-            bufferBlock = parseInt(bufferBlock.subarray(CAM_INIT_BYTE_LENGTH, imei_byte_start).toString('hex'))
+            bufferBlock = parseInt(bufferBlock.subarray(CAM_INIT_BYTE_LENGTH, imei_byte_start).toString('hex'), 16)
             console.log('cam imei??', typeof bufferBlock, bufferBlock)
             console.log('cam settings??', settings.toString('hex'), parseInt(settings.toString('hex')))
             define_hex_file_types_for_records_flush()
