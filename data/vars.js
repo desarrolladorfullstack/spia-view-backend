6cd47d42589d27c7f049f6dc1304fbd335fbe9c4
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2022-12-20 15:21:36 -0500

    Data sender part 2.2
    the_vars: module.exports

--- data/vars.js
+++ data/vars.js
@@ -1,9 +1,12 @@
-export const HEX = 'hex'
-export const RADIX_HEX = 16
-export const BYTE_ZERO = 0x0000
-export const UTF8_SETTING = {encoding: 'utf-8'}
-export const INVALID = {'date':'Invalid Date'}
-export const MIME_TYPES = {
-    'image':"image/jpeg",
-    'video_raw':"application/octet-stream"
+
+module.exports = {
+    'HEX':'hex',
+    'RADIX_HEX':16,
+    'BYTE_ZERO':0x0000,
+    'UTF8_SETTING':{encoding: 'utf-8'},
+    'INVALID':{'date':'Invalid Date'},
+    'MIME_TYPES':{
+        'image':"image/jpeg",
+        'video_raw':"application/octet-stream"
+    }
 }
