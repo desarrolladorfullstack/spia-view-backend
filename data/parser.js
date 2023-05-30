bc465aea908add26da82df4f2991e788eb791db6
DESARROLLADOR FULLSTACK <desarrolladorweb@spia.co>
2023-05-30 15:28:24 -0500

    GPRS command part 4.2.3
    FIX 5
    analyse_block TEST 4 isResponseBlock
     Buffer ... equals() if value is 0

--- data/parser.js
+++ data/parser.js
@@ -600,8 +600,8 @@ function analyse_block (bufferBlock) {
         console.log(recent_device.toString())
         return CAM_COMMANDS["init"](cam_mode)
     }
-    let isResponseBlock = bufferBlock.subarray(8, 9).equals(the_vars.CMD.RESPONDING)
-    isResponseBlock &= bufferBlock.subarray(10, 11).equals(the_vars.CMD.TYPE.RECEIVE)
+    let isResponseBlock = (0 === bufferBlock.subarray(8, 9).equals(the_vars.CMD.RESPONDING))
+    isResponseBlock &= (0 === bufferBlock.subarray(10, 11).equals(the_vars.CMD.TYPE.RECEIVE))
     console.log("isResponseBlock??:", isResponseBlock,
         bufferBlock.subarray(8, 9), the_vars.CMD.RESPONDING.equals(bufferBlock.subarray(8, 9)),
         bufferBlock.subarray(10, 11), the_vars.CMD.TYPE.RECEIVE.equals(bufferBlock.subarray(10, 11))
